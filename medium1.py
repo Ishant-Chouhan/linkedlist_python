9fd8e2fe7f2a4d87a6504a9c66f26434695242ab
Ishant_Chouhan <ishantchouhan666@gmail.com>
2024-07-26 10:27:37 +0530

    final code

--- medium1.py
+++ medium1.py
@@ -23,48 +23,55 @@ class linkedlist:
             current=current.link
     
     def grp_rev(self,k):
-        current=self.head
-        temp_current=self.head
-        count=1
-        while current!=None:
-            if count==0:
-                temp_current=current
-                current=current.link
-                count+=1
-            elif count==k:
-                current2=current
-                while True:
+        if k==1:
+            self.print()
+        elif k>1:
+            current=self.head
+            temp_current=self.head
+            count=1
+            while current!=None:
+                if count==0:
+                    temp_current=current
+                    current=current.link
+                    count+=1
+                elif count==k:
+                    current2=current
+                    while True:
+                        temp=temp_current
+                        while temp.link!=current:
+                            temp=temp.link
+                        if temp==temp_current:
+                            print(current.data)
+                            if temp_current==self.head:
+                                print(temp_current.data)
+                            break
+                        else:
+                            print(current.data)
+                            current=temp
+                    
+                    current=current2
+                    count=0
+                elif current.link==None and (count>0 and count<k):
                     temp=temp_current
-                    while temp.link!=current:
-                        temp=temp.link
-                    if temp==temp_current:
-                        print(current.data)
-                        if temp_current==self.head:
-                            print(temp_current.data)
-                        break
-                    else:
-                        print(current.data)
-                        current=temp
-                
-                current=current2
-                count=0
-            elif current.link==None and (count>0 and count<k):
-                while True:
-                    temp=temp_current
-                    while temp.link!=current:
-                        temp=temp.link
-                    if temp==temp_current:
-                        print(current.data)
-                        break
-                    else:
-                        print("curent",current.data)
-                        current=temp
-            else:
-                current=current.link
-                count+=1
+                    while current!=temp_current:
+                        if temp.link==current:
+                            print(current.data)
+                            current=temp
+                            temp=temp_current
+                        else:
+                            temp=temp.link
+                    break
+                else:
+                    current=current.link
+                    count+=1
+        else:
+            print("Oops...Given number should be greater than 0...!!")
 
 l=linkedlist()
 while True:
+    print("""press 1 to perform insertion
+press 2 to display the list
+press 3 to perform group reversal""")
     choice=int(input("Enter your choice: "))
     if choice==1:
         l.add(node(input("Enter data: ")))
